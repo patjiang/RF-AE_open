b4791b743cbd62414be8684404fa8d163a790321
Patrick Jiang <patjiang04@gmail.com>
2025-09-22 13:55:59 -0700

    Update loss.py

--- loss.py
+++ loss.py
@@ -11,9 +11,11 @@ def reconstruction_loss(p, p_hat):
   kl_p_hat_m = F.kl_div(p_hat.log(), m)
   return (kl_pm.add(kl_p_hat_m)).div(2)
 
+
 def geom_reg_loss(z, z_hat):
   return ((z.sub(z_hat))**2)).sum(dim=list(range(1, z.dim())))
-  
+
+
 def compound_loss(ps, zs, r_lambda):
   p, ph = ps
   z, zh = zs
