cb8102ee6a5d3aa29d2d4b917dee9fb9e98a2323
Patrick Jiang <patjiang04@gmail.com>
2025-09-22 14:11:41 -0700

    Update train.py

--- train.py
+++ train.py
@@ -14,7 +14,7 @@ def train_step(AE, data):
   return loss
   
 #TODO: Replace with datast object that uses RF-GAP from raw data to prob. measures
-def train(inp_size=800, epochs = 200, dataset = torch.randn(1000, 800).to(device)):
+def train(inp_size=800, epochs = 200, dataset = torch.randn(1000, 800)):
   device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
   temp_model = AE(inp_size).to(device)
   optim = torch.optim.Adam(temp_model.parameters(), lr = 1e-3)
@@ -23,7 +23,7 @@ def train(inp_size=800, epochs = 200, dataset = torch.randn(1000, 800).to(device
     epclosses = []
     for data in dataset:
       optim.zero_grad()
-      loss = train_step(AE, data)
+      loss = train_step(AE, data.to(device))
       loss.backward()
       optim.step()
       epclosses.append(loss.detach().cpu().item())
