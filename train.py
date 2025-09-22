0d2b1bc469250a1a18c77923568b08a2182778f6
Patrick Jiang <patjiang04@gmail.com>
2025-09-22 14:02:42 -0700

    Update train.py

--- train.py
+++ train.py
@@ -1,5 +1,7 @@
 import torch
 from torch.utils.data import DataLoader
+from tqdm import tqdm
+import statistics
 
 from ae import *
 from loss import *
@@ -7,10 +9,25 @@ from loss import *
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 temp_model = AE().to(device)
 optim = torch.optim.Adam(temp_model.parameter(), lr = 1e-3)
+
 epochs = 200
+
+#TODO: Replace with datast object that uses RF-GAP from raw data to prob. measures
 dataset = torch.randn(1000, 800).to(device)
 
-for epoch in range(epochs):
+
+for epoch in tqdm(range(epochs)):
+  epclosses = []
   for data in dataset:
+    optim.zero_grad()
     recon, z = AE(data)
-    #ergh gotta go to next class
+    #@TODO: Replace with RF-PHATE
+    dummy_z = torch.randn(1, 2).to(device)
+    loss = compound_loss((data, recon), (z, dummy_z))
+    loss.backward()
+    optim.step()
+    epclosses.append(loss.detach().cpu().item())
+  mu, var = statistics.mean(epclosses), statistics.stdev(epclosses)
+  printf('epoch {epoch}, mean = {mu:.3f}, var = {var:.3f}')
+
+  
